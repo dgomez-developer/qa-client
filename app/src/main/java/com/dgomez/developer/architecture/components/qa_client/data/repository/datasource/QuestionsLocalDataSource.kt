234274a405f09a74c5bbb2fb737c2abc1cbf3d5b
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2019-11-02 17:54:34 +0100

    Using transformations in persistence data source

--- app/src/main/java/com/dgomez/developer/architecture/components/qa_client/data/repository/datasource/QuestionsLocalDataSource.kt
+++ app/src/main/java/com/dgomez/developer/architecture/components/qa_client/data/repository/datasource/QuestionsLocalDataSource.kt
@@ -1,22 +1,26 @@
 package com.dgomez.developer.architecture.components.qa_client.data.repository.datasource
 
-import android.content.Context
+import androidx.arch.core.util.Function
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
-import androidx.room.Room
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.dgomez.developer.architecture.components.qa_client.data.database.AppDatabase
 import com.dgomez.developer.architecture.components.qa_client.data.model.QuestionEntity
 import com.dgomez.developer.architecture.components.qa_client.domain.Question
 
 class QuestionsLocalDataSource(val db: AppDatabase) {
 
-    fun getAllQuestions(): LiveData<List<Question>> = MediatorLiveData<List<Question>>().apply {
-        val dbLD = db.questionsDao().getAll()
-        this.addSource(dbLD) {
-            val questions = it.map { dbquestion -> Question(dbquestion.id, dbquestion.question, dbquestion.contact) }
-            this.postValue(questions)
-        }
-    }
+//    fun getAllQuestions(): LiveData<List<Question>> = MediatorLiveData<List<Question>>().apply {
+//        val dbLD = db.questionsDao().getAll()
+//        this.addSource(dbLD) {
+//            val questions = it.map { dbquestion -> Question(dbquestion.id, dbquestion.question, dbquestion.contact) }
+//            this.postValue(questions)
+//        }
+//    }
+
+    fun getAllQuestions() = Transformations.map(db.questionsDao().getAll()) {
+            input -> input.map{ dbquestion -> Question(dbquestion.id, dbquestion.question, dbquestion.contact) } }
 
     fun updateQuestions(questions: List<Question>) {
         db.questionsDao().insertAll(questions.map { QuestionEntity(it.id, it.question, it.contact) })
